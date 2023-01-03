3240711c095bcbc9d0a087beb05d0c5dee2ea746
poojashri <poojashri23>
2023-01-03 10:38:52 +0530

    Calculation Coding

--- Calcualtion.aspx
+++ Calcualtion.aspx
@@ -34,49 +34,49 @@
             </tr>
             <tr>
                 <td style="padding-left:100px;padding-top: 10px;">
-                    <asp:Button ID="btn1" Text="1" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn1" Text="1" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn1_Click"
                         Width="57px" />
-                    <asp:Button ID="btn2" Text="2" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn2" Text="2" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn2_Click"
                         Width="57px" />
-                    <asp:Button ID="btn3" Text="3" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn3" Text="3" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn3_Click"
                         Width="57px" />
-                    <asp:Button ID="btnadd" Text="+" runat="server" Height="37px" Style="margin-left: 0px; margin-top: 0px;"
-                        Width="57px" />
-                </td>
-            </tr>
-            <tr>
-                <td style="padding-left:100px">
-                    <asp:Button ID="btn4" Text="4" runat="server" Height="37px" Style="margin-left: 0px"
-                        Width="57px" />
-                    <asp:Button ID="btn5" Text="5" runat="server" Height="37px" Style="margin-left: 0px"
-                        Width="57px" />
-                    <asp:Button ID="btn6" Text="6" runat="server" Height="37px" Style="margin-left: 0px"
-                        Width="57px" />
-                    <asp:Button ID="sub" Text="-" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btnadd" Text="+" runat="server" Height="37px" Style="margin-left: 0px;" OnClick="btnadd_Click"
                         Width="57px" />
                 </td>
             </tr>
             <tr>
                 <td style="padding-left:100px">
-                    <asp:Button ID="btn7" Text="7" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn4" Text="4" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn4_Click"
                         Width="57px" />
-                    <asp:Button ID="btn8" Text="8" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn5" Text="5" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn5_Click"
                         Width="57px" />
-                    <asp:Button ID="btn9" Text="9" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn6" Text="6" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn6_Click"
                         Width="57px" />
-                    <asp:Button ID="btnmul" Text="*" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="sub" Text="-" runat="server" Height="37px" Style="margin-left: 0px" OnClick="sub_Click"
                         Width="57px" />
                 </td>
             </tr>
             <tr>
                 <td style="padding-left:100px">
-                    <asp:Button ID="btn0" runat="server" Text="0" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn7" Text="7" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn7_Click"
                         Width="57px" />
-                    <asp:Button ID="btnclr" runat="server" Text="CLR" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn8" Text="8" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn8_Click"
                         Width="57px" />
-                    <asp:Button ID="btneql" runat="server" Text="=" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btn9" Text="9" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btn9_Click"
                         Width="57px" />
-                    <asp:Button ID="btndiv" Text="/" runat="server" Height="37px" Style="margin-left: 0px"
+                    <asp:Button ID="btnmul" Text="*" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btnmul_Click"
+                        Width="57px" />
+                </td>
+            </tr>
+            <tr>
+                <td style="padding-left:100px">
+                    <asp:Button ID="btn0" runat="server" Text="0" Height="37px" Style="margin-left: 0px" OnClick="btn0_Click"
+                        Width="57px" />
+                    <asp:Button ID="btnclr" runat="server" Text="CLR" Height="37px" Style="margin-left: 0px" OnClick="btnclr_Click"
+                        Width="57px" />
+                    <asp:Button ID="btneql" runat="server" Text="=" Height="37px" Style="margin-left: 0px" OnClick="btneql_Click"
+                        Width="57px" />
+                    <asp:Button ID="btndiv" Text="/" runat="server" Height="37px" Style="margin-left: 0px" OnClick="btndiv_Click"
                         Width="57px" />
                 </td>
             </tr>
--- Calcualtion.aspx.cs
+++ Calcualtion.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +10,314 @@ namespace Caculator
 {
     public partial class Calcualtion : System.Web.UI.Page
     {
+        static float a, c, d;
+        static char b;
         protected void Page_Load(object sender, EventArgs e)
         {
+         //   txtnumber.Text = "";
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn1.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn1.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn1_Click" + ex.Message);
+            }
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn2.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn2.Text;
+
+            }
+             catch (Exception ex)
+            {
+                Console.Write("btn2_Click" + ex.Message);
+            }
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn3.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn3.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn3_Click" + ex.Message);
+            }
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn4.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn4.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn4_Click" + ex.Message);
+            }
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn5.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn5.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn5_Click" + ex.Message);
+            }
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn6.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn6.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn6_Click" + ex.Message);
+            }
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn7.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn7.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn7_Click" + ex.Message);
+            }
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn8.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn8.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn8_Click" + ex.Message);
+            }
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn9.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn9.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn9_Click" + ex.Message);
+            }
+        }
+
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtnumber.Text == "+") || (txtnumber.Text == "-") || (txtnumber.Text == "*") || (txtnumber.Text == "/"))
+                {
+                    txtnumber.Text = "";
+                    txtnumber.Text = txtnumber.Text + btn0.Text;
+                }
+                else
+                    txtnumber.Text = txtnumber.Text + btn0.Text;
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btn0_Click" + ex.Message);
+            }
+        }
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               // var bb = txtnumber.Text;
+                a = Convert.ToInt32(txtnumber.Text);
+                txtnumber.Text = "";
+                b = '+';
+                txtnumber.Text += b;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btnadd_Click" + ex.Message);
+            }
+        }
+
+        protected void sub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToInt32(txtnumber.Text);
+                txtnumber.Text = "";
+                b = '-'; 
+                txtnumber.Text += b;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("sub_Click" + ex.Message);
+            }
 
         }
+
+        protected void btnmul_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToInt32(txtnumber.Text);
+                txtnumber.Text = "";
+                b = '*';
+                txtnumber.Text += b;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("sub_Click" + ex.Message);
+            }
+        }
+        protected void btndiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = Convert.ToInt32(txtnumber.Text);
+                txtnumber.Text = "";
+                b = '/';
+                txtnumber.Text += b;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("sub_Click" + ex.Message);
+            }
+        }
+
+        protected void btnclr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtnumber.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Console.Write("btnclr_Click" + ex.Message);
+            }
+        }
+
+        protected void btneql_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                c = Convert.ToInt32(txtnumber.Text);
+                txtnumber.Text = "";
+                if (b == '/')
+                {
+                    d = a / c;
+                    txtnumber.Text += d;
+                    a = d;
+                }
+                else if (b == '+')
+                {
+                    d = a + c;
+                    txtnumber.Text += d;
+                    a = d;
+                }
+                else if (b == '-')
+                {
+                    d = a - c;
+                    txtnumber.Text += d;
+                    a = d;
+                }
+                else
+                {
+                    d = a * c;
+                    txtnumber.Text += d;
+                    a = d;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write("sub_Click" + ex.Message);
+            }
+
+        }
+
+      
     }
 }
